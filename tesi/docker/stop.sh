0521ec9dcba851389e90ca81263b2806abb5aea5
ceciliacal <54946836+ceciliacal@users.noreply.github.com>
2022-03-17 13:23:29 +0100

    Delete tesi directory

--- tesi/docker/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo docker stop zookeeper
-sudo docker stop kafka
-sudo docker stop kafdrop
-sudo docker stop jobmanager
-sudo docker stop docker_taskmanager_3
-sudo docker stop docker_taskmanager_2
-sudo docker stop docker_taskmanager_1
