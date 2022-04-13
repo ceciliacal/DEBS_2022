135f52788c65a392629ad629562491656d811787
ceciliacal <c.calavaro@yahoo.it>
2022-04-13 20:23:55 +0200

    docker update

--- thesis/docker/stop.sh
+++ thesis/docker/stop.sh
@@ -3,7 +3,4 @@
 sudo docker stop zookeeper
 sudo docker stop kafka
 sudo docker stop kafdrop
-sudo docker stop jobmanager
-sudo docker stop docker_taskmanager_3
-sudo docker stop docker_taskmanager_2
-sudo docker stop docker_taskmanager_1
+
